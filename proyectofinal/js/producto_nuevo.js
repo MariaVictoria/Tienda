8d16e1d9278999e9a2a2c968aadb3e629e8b3fd8
Viktoria <123131515+MariaVictoria@users.noreply.github.com>
2023-07-13 20:17:50 -0300

    Update producto_nuevo.js

--- proyectofinal/js/producto_nuevo.js
+++ proyectofinal/js/producto_nuevo.js
@@ -1,44 +1,40 @@
 const app = Vue.createApp({
   data() {
     return {
-      fecha: '',
-      datosCliente: '',
-      producto: '',
-      imagenUrl: '',
-      imagenPreview: '',
-      cantidad: '',
-      seña: '',
-      fechaEntrega: '',
-      consultaEstado: '',
-      url: 'http://localhost:5500/pedido',
+      producto: {
+        nombre: '',
+        descripcion: '',
+        precio: '',
+        stock: '',
+        imagenUrl: '',
+        imagenPreview: '',
+      },
+      url: 'http://localhost:5500/producto',
     };
   },
   methods: {
     handleFileUpload(event) {
       const file = event.target.files[0];
-      this.imagenUrl = URL.createObjectURL(file);
-      this.imagenPreview = this.imagenUrl;
+      this.producto.imagenUrl = URL.createObjectURL(file);
+      this.producto.imagenPreview = this.producto.imagenUrl;
     },
     grabar() {
-      const nuevoPedido = {
-        fecha: this.fecha,
-        datosCliente: this.datosCliente,
-        producto: this.producto,
-        imagen: this.imagenUrl,
-        cantidad: this.cantidad,
-        seña: this.seña,
-        fechaEntrega: this.fechaEntrega,
-        consultaEstado: this.consultaEstado,
+      const nuevoProducto = {
+        nombre: this.producto.nombre,
+        descripcion: this.producto.descripcion,
+        precio: this.producto.precio,
+        stock: this.producto.stock,
+        imagenUrl: this.producto.imagenUrl,
       };
 
       axios
-        .post(this.url, nuevoPedido)
+        .post(this.url, nuevoProducto)
         .then(response => {
-          console.log('Nuevo pedido registrado:', response.data);
-          // Aquí puedes realizar acciones adicionales después de guardar el pedido, como mostrar una notificación de éxito o redirigir a otra página.
+          console.log('Nuevo producto registrado:', response.data);
+          // Aquí puedes realizar acciones adicionales después de guardar el producto, como mostrar una notificación de éxito o redirigir a otra página.
         })
         .catch(error => {
-          console.error('Error al registrar el pedido:', error);
+          console.error('Error al registrar el producto:', error);
         });
     },
   },
