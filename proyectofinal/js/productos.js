5f158847512ccf5e24b40c324f334955b400b0a7
Viktoria <123131515+MariaVictoria@users.noreply.github.com>
2023-07-13 23:14:03 -0300

    Update productos.js

--- proyectofinal/js/productos.js
+++ proyectofinal/js/productos.js
@@ -1,104 +1,30 @@
-const { createApp } = Vue
+const { createApp } = Vue;
+
 createApp({
-    data(){
-        return {
-            amigurumi: [],
-            url: 'http://mviktoriau.pythonanywhere.com/amigurumi',
-            error: false,
-            cargando: true,
-            id: 0,
-            nombreProducto: '',
-            imagen: '',
-            descripcion: '',
-            precio: 0,
-            stock: 0,
-            imagenPreview: '', 
-            }
-        },
-        methods: {
-            fetchData(url) {
-                fetch(url)
-                    .then(response => response.json())
-                    .then(data => {
-                        this.amigurumi = data;
-                        this.cargando = false
-                    })
-                    .catch(err => {
-                        console.error(err);
-                        this.error = true
-                })
-        },
-        eliminar(amigurumi) {
-                const url = this.url + '/' + amigurumi
-                var options = {
-                    method: 'DELETE',
-                        }
-                Swal.fire({
-                    title: '¿Estas seguro que queres eliminar este producto?',
-                    text: "",
-                    icon: 'warning',
-                    showCancelButton: true,
-                    confirmButtonColor: '#3085d6',
-                    cancelButtonColor: '#d33',
-                    confirmButtonText: 'Si, eliminar!'
-                    })
-                    .then((result) => {
-                        if (result.isConfirmed){
-                            Swal.fire(
-                                'Producto eliminado',  
-                                '',                  
-                                'success'
-                            )
-                            fetch(url, options)
-                                .then(res => res.text()) 
-                                .then(res => {
-                                    setTimeout(time=>{location.reload()},1200)
-                                })
-                        }
-                    })           
-        },       
-       grabar() {
-                let amigurumi = {
-                    nombre: this.nombre,
-                    precio: this.precio,
-                    stock: this.stock,
-                    imagen: this.imagen
-                }
-                if (amigurumi.nombre ==''){
-                    Swal.fire(
-                        'Ha surgido un error',
-                        'El producto debe tener al menos un nombre',
-                        'error'
-                    )
-                } else {
-                    var options = {
-                        body: JSON.stringify(amigurumi),
-                        method: 'POST',
-                        headers: { 'Content-Type': 'application/json' },
-                        redirect: 'follow'
-                    }
-                    fetch(this.url, options)
-                        .then(function () {         
-                                Swal.fire(
-                                    'Producto dado de alta',
-                                    '',
-                                    'success'
-                                )
-                                setTimeout(time=>{window.location.href = "./productos.html";},1200)
-                            })
-                        .catch(err => {
-                            console.error(err);
-                            Swal.fire(
-                                'Error el grabar',
-                                '',
-                                'error'
-                            )
-                            setTimeout(time=>{window.location.href = "./productos.html";},1200)
-                        })
-                }
-            }
+  data() {
+    return {
+      error: false,
+      loading: true,
+      amigurumis: [],
+    };
+  },
+  methods: {
+    fetchData() {
+      fetch('http://mviktoriau.pythonanywhere.com/amigurumi')
+        .then((response) => response.json())
+        .then((data) => {
+          this.loading = false;
+          this.amigurumis = data;
+        })
+        .catch((error) => {
+          console.error(error);
+          this.error = true;
+          this.loading = false;
+        });
     },
-      created() {
-        this.fetchData(this.url)
-    },
-}).mount('#app')
+  },
+  created() {
+    this.fetchData();
+  },
+}).mount('#app');
+      
