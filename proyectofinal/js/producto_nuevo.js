f570ce8d9103057a8a814ecb7634a4a163f2811f
Ma_Victoria <victoriacursos2022@gmail.com>
2023-07-14 18:26:53 -0300

    salto de fe

--- proyectofinal/js/producto_nuevo.js
+++ proyectofinal/js/producto_nuevo.js
@@ -1,70 +1,101 @@
 console.log(location.search)
-var id = location.search.substr(4);
+var id=location.search.substr(4)
 console.log(id)
-// let valorid = document.getElementById("id");
-// valorid.innerHTML = `${id}`;
 
 const { createApp } = Vue
-const app = createApp({
+createApp({
   data() {
     return {
-      // amigurumi: {
-        // idnombre: '',
-        // codigo: '',
-        nombre: "",
-        // descripcion: '',
-        precio: 0,
-        stock: 0,
-        imagenUrl: "",
-        // imagenPreview: "",
-        url: 'https://mviktoriau.pythonanywhere.com/amigurumi'+id, 
-          
-      
-    };
-  },
+      id:0,
+      nombre:"",
+      descripcion:'',
+      imagen:"",
+      stock:0,
+      precio:0,
+      url:'https://mviktoriau.pythonanywhere.com/amigurumi/'+id,
+      }
+      },
+
   methods: {
-    fetchData(url) {
-    fetch(url)
-    .then(response => response.json())
-    .then(data => {
-    
-    console.log(data)
-    this.id=data.id
-    this.nombre = data.nombre;
-    this.precio=data.precio
-    this.stock=data.stock
-    this.imagenUrl=data.imagenUrl
-    })
-    .catch(err => {
-    console.error(err);
-    this.error=true
-    })
+    handleFileUpload(event) {
+      const file = event.target.files[0];
+      this.amigurumi.imagenUrl = URL.createObjectURL(file);
+      this.amigurumi.imagenPreview = this.amigurumi.imagenUrl;
     },
-    grabar(){
-      let amigurumi = {
+    methods: {
+      fetchData(url) {
+      fetch(url)
+      .then(response => response.json())
+      .then(data => {
+      
+      console.log(data)
+      this.id=data.id
+      this.nombre = data.nombre;
+      this.descripcion=data.descripcion;
+      this.imagen=data.imagen
+      this.stock=data.stock
+      this.precio=data.precio
+      })
+      .catch(err => {
+      console.error(err);
+      this.error=true
+      })
+      },
+      modificar() {
+      let producto = {
       nombre:this.nombre,
+      descripcion:this.descripcion,
       precio: this.precio,
       stock: this.stock,
-      imagenUrl:this.aimagenUrl
+      imagen:this.imagen
       }
       var options = {
-      body:JSON.stringify(amigurumi),
-      method: 'POST',
+      body: JSON.stringify(producto),
+      method: 'PUT',
       headers: { 'Content-Type': 'application/json' },
       redirect: 'follow'
       }
       fetch(this.url, options)
       .then(function () {
-      alert("Registro grabado")
-      window.location.href = "./productos.html";
+      alert("Registro modificado")
+      window.location.href = "../templates/productos.html";
       })
       .catch(err => {
       console.error(err);
-      alert("Error al Grabarr")
+      alert("Error al Modificar")
       })
       }
       },
       created() {
       this.fetchData(this.url)
       },
-      }).mount('#app')
+      
+    grabar() {
+      let amigurumi = {
+        nombre: this.amigurumi.nombre,
+        precio: this.amigurumi.precio,
+        description: this.amigurumi.descripcion,
+        stock: this.amigurumi.stock,
+        imagenUrl: this.amigurumi.imagen
+      }
+      var options = {
+        body: JSON.stringify(amigurumi),
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        redirect: 'follow'
+      }
+      fetch(this.url, options)
+        .then(function () {
+          alert("Registro grabado")
+          window.location.href = "./productos.html";
+        })
+        .catch(err => {
+          console.error(err);
+          alert("Error al Grabarr")
+        })
+    }
+  },
+  created() {
+    this.fetchData(this.url)
+  },
+}).mount('#app')
---
+++ proyectofinal/js/productos3.js
@@ -0,0 +1,116 @@
+import appModal from './modal.js';
+
+const { createApp } = Vue
+//var endPoint = 'https://mviktoriau.pythonanywhere.com/';
+//var endPoint = 'http://127.0.0.1:5000/';
+createApp({
+    data() {
+
+        return {
+            datos: '',
+            amigurumi: [],
+            url: endPoint+'/amigurumi', // si ya lo subieron a pythonanywhere
+            error: false,
+            cargando: true,
+            /*atributos para el guardar los valores del formulario */
+            id: 0,
+            nombre: "",
+            imagen: "",
+            stock: 0,
+            precio: 0,
+            modalImageURL:''
+        }
+    },
+    methods: {
+        fetchData(url) {
+            fetch(url)
+
+                .then(response => response.json())
+                .then(data => {
+                    this.amigurumi = data;
+                    this.cargando = false
+                })
+                .catch(err => {
+                    console.error(err);
+                    this.error = true
+                })
+        },
+
+        eliminar(amigurumi) {
+            const url = this.url + '/' + amigurumi;
+            var options = {
+                method: 'DELETE',
+            }
+            fetch(url, options)
+                .then(res => res.text()) // or res.json()
+                .then(res => {
+                    location.reload();
+                })
+        },
+        grabar() {
+            let amigurumi = {
+                nombre: this.nombre,
+                precio: this.precio,
+                stock: this.stock,
+                imagen: this.imagen
+            }
+            var options = {
+                body: JSON.stringify(amigurumi),
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                redirect: 'follow'
+            }
+            fetch(this.url, options)
+                .then(function () {
+                    alert("Registro grabado")
+                    window.location.href = "./productos.html";
+                })
+                .catch(err => {
+                    console.error(err);
+                    alert("Error al Grabarr")
+
+                })
+        },
+        openModal(imageURL) {
+            // Actualizar la URL de la imagen en el modal
+            document.getElementById('modalImage').src = imageURL;
+        
+            // Obtener el elemento del modal
+            var modalElement = document.getElementById('imageModal');
+        
+            // Mostrar el modal
+            modalElement.classList.add('show');
+            modalElement.style.display = 'block';
+            document.body.classList.add('modal-open');
+        
+            // Remover la clase 'show' y ocultar el modal al hacer clic fuera de él
+            modalElement.addEventListener('click', function(event) {
+              if (event.target === modalElement) {
+                modalElement.classList.remove('show');
+                modalElement.style.display = 'none';
+                document.body.classList.remove('modal-open');
+              }
+            });
+          }
+    },
+    created() {
+        this.fetchData(this.url)
+    },
+})
+.component('image-modal', appModal.component('image-modal'))
+/*
+.component('image-modal', {
+    template: `
+      <div id="imageModal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="imageModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-body">
+              <img id="modalImage" :src="imageURL" alt="Modal Image">
+            </div>
+          </div>
+        </div>
+      </div>
+    `,
+    props: ['imageURL']
+  })*/
+  .mount('#app')
