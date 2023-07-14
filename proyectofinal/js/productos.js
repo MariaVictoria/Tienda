7e5de843a99c1e7c84db93837bfad0fa49d5d627
Viktoria <123131515+MariaVictoria@users.noreply.github.com>
2023-07-14 05:16:39 -0300

    Update productos.js

--- proyectofinal/js/productos.js
+++ proyectofinal/js/productos.js
@@ -1,37 +1,29 @@
-document.getElementById("header").innerHTML=` <nav class="navbar navbar-expand-sm
-navbar-light bg-light">
-<div class="container">
-<a class="navbar-brand" href="./productos.html">CRUD</a>
-
-</div>
-</nav>`
-const app = Vue.createApp({
-  data() {
-    return {
-      error: false,
-      loading: true,
-      amigurumis: [],
-    };
-  },
-  methods: {
-    fetchData() {
-      axios
-        .get('http://mviktoriau.pythonanywhere.com/amigurumi')
-        .then((response) => {
-          this.loading = false;
-          this.amigurumis = response.data;
-        })
-        .catch((error) => {
-          console.error(error);
-          this.error = true;
-          this.loading = false;
+ const app = Vue.createApp({
+            data() {
+                return {
+                    error: false,
+                    loading: true,
+                    amigurumis: [],
+                };
+            },
+            methods: {
+                fetchData() {
+                    axios
+                        .get('https://mviktoriau.pythonanywhere.com/amigurumi')
+                        .then((response) => {
+                            this.loading = false;
+                            this.amigurumis = response.data;
+                        })
+                        .catch((error) => {
+                            console.error(error);
+                            this.error = true;
+                            this.loading = false;
+                        });
+                },
+            },
+            created() {
+                this.fetchData();
+            },
         });
-    },
-  },
-  created() {
-    this.fetchData();
-  },
-});
 
-app.mount('#app');
-
+        app.mount('#app');
