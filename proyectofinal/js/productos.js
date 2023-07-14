143543edd85b86bd047bcb92f8e25534885c68be
Viktoria <123131515+MariaVictoria@users.noreply.github.com>
2023-07-14 14:47:49 -0300

    Update productos.js

--- proyectofinal/js/productos.js
+++ proyectofinal/js/productos.js
@@ -1,4 +1,4 @@
-   document.addEventListener("DOMContentLoaded", function () {
+     document.addEventListener("DOMContentLoaded", function () {
             const app = Vue.createApp({
                 data() {
                     return {
