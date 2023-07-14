3acec233b24ac2fb49c38109641713d363b1a96f
Viktoria <123131515+MariaVictoria@users.noreply.github.com>
2023-07-14 05:28:42 -0300

    Update productos.js

--- proyectofinal/js/productos.js
+++ proyectofinal/js/productos.js
@@ -1,29 +1,31 @@
- const app = Vue.createApp({
-            data() {
-                return {
-                    error: false,
-                    loading: true,
-                    amigurumis: [],
-                };
-            },
-            methods: {
-                fetchData() {
-                    axios
-                        .get('https://mviktoriau.pythonanywhere.com/amigurumi')
-                        .then((response) => {
-                            this.loading = false;
-                            this.amigurumis = response.data;
-                        })
-                        .catch((error) => {
-                            console.error(error);
-                            this.error = true;
-                            this.loading = false;
-                        });
-                },
-            },
-            created() {
-                this.fetchData();
-            },
-        });
+document.addEventListener("DOMContentLoaded", function() {
+  const app = Vue.createApp({
+    data() {
+      return {
+        error: false,
+        loading: true,
+        amigurumis: [],
+      };
+    },
+    methods: {
+      fetchData() {
+        axios
+          .get('https://mviktoriau.pythonanywhere.com/amigurumi')
+          .then((response) => {
+            this.loading = false;
+            this.amigurumis = response.data;
+          })
+          .catch((error) => {
+            console.error(error);
+            this.error = true;
+            this.loading = false;
+          });
+      },
+    },
+    created() {
+      this.fetchData();
+    },
+  });
 
-        app.mount('#app');
+  app.mount('#app');
+});
