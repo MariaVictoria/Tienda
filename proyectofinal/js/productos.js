8760b8623d8b08fbfecf366deabb828ef1fd832d
Viktoria <123131515+MariaVictoria@users.noreply.github.com>
2023-07-14 01:28:01 -0300

    Update productos.js

--- proyectofinal/js/productos.js
+++ proyectofinal/js/productos.js
@@ -1,3 +1,10 @@
+document.getElementById("header").innerHTML=` <nav class="navbar navbar-expand-sm
+navbar-light bg-light">
+<div class="container">
+<a class="navbar-brand" href="./productos.html">CRUD</a>
+
+</div>
+</nav>`
 const app = Vue.createApp({
   data() {
     return {
