a5438e22b7aea6f20a9b5c5d29b922e0cac2ee49
Viktoria <123131515+MariaVictoria@users.noreply.github.com>
2023-07-17 16:24:19 -0300

    Update app.py

--- proyectofinal/app.py
+++ proyectofinal/app.py
@@ -11,7 +11,7 @@ CORS(app) #modulo cors es para que me permita acceder desde el frontend al backe
 app = Flask(__name__)
 CORS(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://MViktoriaU:Delfines/2@MViktoriaU.mysql.pythonanywhere-services.com/MViktoriaU$Tienda'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://MViktoriaU:***@MViktoriaU.mysql.pythonanywhere-services.com/MViktoriaU$Tienda'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False #none
 
 db= SQLAlchemy(app)   #crea el objeto db de la clase SQLAlquemy
